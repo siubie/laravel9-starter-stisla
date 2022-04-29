a8f8b97c6fd9a2c3d45e167545d4c5ce81784e33
siubie <putraprima@gmail.com>
2022-04-29 11:14:16 +0700

    Test Paging

--- tests/Feature/UserListTest.php
+++ tests/Feature/UserListTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class UserListTest extends TestCase
@@ -65,5 +66,73 @@ class UserListTest extends TestCase
 
         //keliatan paging 11
         $response->assertSeeText("11");
+    }
+
+    public function test_user_can_search_and_result_shown_in_list()
+    {
+
+        $this->actingAs(User::find(1));
+        $response = $this->get("user-management/user", [
+            "name" => "user"
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertSeeText("user");
+        $response->assertSeeText("user@gmail.com");
+
+        $response = $this->get("user-management/user", [
+            "name" => "SuperAdmin"
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertSeeText("SuperAdmin");
+        $response->assertSeeText("superadmin@gmail.com");
+    }
+
+    public function test_user_can_search_and_result_shown_in_list_with_paging()
+    {
+
+        //login admin
+        $this->actingAs(User::find(1));
+
+        //generate 15 user
+        for ($i = 0; $i < 15; $i++) {
+            User::create(
+                [
+                    "name" => "user" . $i,
+                    'email' => "user" . $i . "@email.com",
+                    'password' => Hash::make('password'),
+                    'email_verified_at' => now(),
+                ]
+            );
+        }
+
+        //response nya di search
+        $response = $this->get("user-management/user", [
+            "name" => "user"
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertSeeText("user");
+        $response->assertSeeText("user@gmail.com");
+
+        // harusnya keliatan halaman paging nya
+        $response->assertSeeTextInOrder(["1", "2"]);
+
+        //dicek ke halaman 2
+        $response = $this->get("user-management/user", [
+            "name" => "user",
+            "page" => "2"
+        ]);
+        //harusnya masi ada paging nya
+        $response->assertSeeTextInOrder(["1", "2"]);
+
+        //dicek ke halaman3
+        $response = $this->get("user-management/user", [
+            "name" => "user",
+            "page" => "3"
+        ]);
+        //harusnya masi ada paging nya
+        $response->assertDontSeeText("<");
     }
 }
