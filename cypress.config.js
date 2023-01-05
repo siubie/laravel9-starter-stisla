f04bd15b3c524e6b947d203724766a017223a770
Putra Prima Arhandi <putraprima@gmail.com>
2023-01-06 05:02:17 +0700

    Setup Test

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    chromeWebSecurity: false,
+    retries: 2,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    defaultCommandTimeout: 5000,
+    watchForFileChanges: false,
+    videosFolder: "tests/cypress/videos",
+    screenshotsFolder: "tests/cypress/screenshots",
+    fixturesFolder: "tests/cypress/fixture",
+    e2e: {
+        setupNodeEvents(on, config) {
+            return require("./tests/cypress/plugins/index.js")(on, config);
+        },
+        baseUrl: "http://localhost:8000/",
+        specPattern: "tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
+        supportFile: "tests/cypress/support/index.js",
+    },
+});
