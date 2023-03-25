fc5001db1eaf50d6f9ffef5ed75cc0c678a4624b
Putra Prima Arhandi <putraprima@gmail.com>
2023-03-25 12:22:41 +0700

    fixing testing env dan sample test

--- tests/Feature/CreateUserTest.php
+++ tests/Feature/CreateUserTest.php
@@ -120,4 +120,56 @@ class CreateUserTest extends TestCase
 
         $response->assertSessionHasErrors(["email" => "The email has already been taken."]);
     }
+
+    //test superadmin can create new user
+    public function test_superadmin_can_create_new_user()
+    {
+        //tambahkan post ke /create
+        $response = $this->post(
+            '/user-management/user',
+            [
+                "name" => "Hora Umum",
+                "email" => "testemail@gmail.com",
+                "password" => Hash::make("password"),
+            ]
+        );
+        // pastikan ga ada error
+        $response->assertSessionHasNoErrors();
+        // pastikan redirect nya bener
+        $response->assertRedirect("/user-management/user");
+        // pastikan keluar notifikasi
+        $response->assertSessionHas("success", "Data Berhasil Ditambahkan");
+        // cek di tabel muncul apa gak
+        $search = $this->get("/user-management/user", [
+            "name" => "Hora Umum",
+        ]);
+        // pastikan tampil emailnya
+        $search->assertSeeText("testemail@gmail.com");
+        // cek di database ada apa gak
+        $this->assertDatabaseHas('users', [
+            "name" => "Hora Umum",
+            "email" => "testemail@gmail.com",
+        ]);
+    }
+
+    //superadmin can delete a user
+    public function test_superadmin_can_delete_a_user()
+    {
+        //create one user using user factory
+        $user = User::factory()->create();
+        //delete the user
+        $response = $this->delete("/user-management/user/" . $user->id);
+        //pastikan ga ada error
+        $response->assertSessionHasNoErrors();
+        //pastikan redirect nya bener
+        $response->assertRedirect("/user-management/user");
+        //pastikan keluar notifikasi
+        $response->assertSessionHas("success", "User Deleted Successfully");
+        //assert not see text $user->name
+        $response->assertDontSeeText($user->name);
+        $this->assertDatabaseMissing('users', [
+            "name" => $user->name,
+            "email" => $user->email,
+        ]);
+    }
 }
